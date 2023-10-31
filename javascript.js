f45cfe6ce837332a1774735a0f5f9dd6a383a7a8
Brian Rainer <brianbulletworks@gmail.com>
2023-10-31 15:18:21 +0700

    Add scoring
    Add button
    Add battle history

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
+let playerScore = 0;
+let computerScore = 0;
+let drawScore = 0;
+
 function getComputerChoice() {
-    const computerChoice = ["Rock", "Paper", "Scissor"];
+    const computerChoice = ["Rock", "Paper", "Scissors"];
     return computerChoice[Math.floor(Math.random() * 3)];
 }
 
@@ -12,30 +16,37 @@ function isPaper(choice) {
 }
 
 function isScissor(choice) {
-    return choice.toLowerCase() == 'scissor';
+    return choice.toLowerCase() == 'scissors';
 }
 
 function playRockPaperScissor(playerSelection, computerChoice) {
-    let result = "Invalid selection, should be Rock, Paper, or Scissor!";
+    let result = "Invalid selection, should be Rock, Paper, or Scissors!";
     if (playerSelection.toLowerCase() == computerChoice.toLowerCase()) {
         result = `Draw! ${playerSelection} and ${computerChoice} is the same!`;
+        drawScore += 1;
     } else if (isRock(playerSelection)) {
         if (isPaper(computerChoice)) {
-            result = `You Lose! ${computerChoice} beats ${playerSelection}`;
+            result = `You Lose! ${playerSelection} lost against ${computerChoice}`;
+            computerScore += 1;
         } else if (isScissor(computerChoice)){
             result = `You Win! ${playerSelection} beats ${computerChoice}`;
+            playerScore += 1;
         }
     } else if (isPaper(playerSelection)) {
         if (isScissor(computerChoice)) {
-            result = `You Lose! ${computerChoice} beats ${playerSelection}`;
+            result = `You Lose! ${playerSelection} lost against ${computerChoice}`;
+            computerScore += 1;
         } else if (isRock(computerChoice)){
             result = `You Win! ${playerSelection} beats ${computerChoice}`;
+            playerScore += 1;
         }
     } else if (isScissor(playerSelection)) {
         if (isRock(computerChoice)) {
-            result = `You Lose! ${computerChoice} beats ${playerSelection}`;
+            result = `You Lose! ${playerSelection} lost against ${computerChoice}`;
+            computerScore += 1;
         } else if (isPaper(computerChoice)){
             result = `You Win! ${playerSelection} beats ${computerChoice}`;
+            playerScore += 1;
         }
     }
     return result
@@ -47,3 +58,30 @@ function game() {
         console.log(playRockPaperScissor(playerSelection, getComputerChoice()))
     }
 }
+
+function updateDisplay() {
+    const playerScoreDisplay = document.querySelector('#playerScore');
+    const computerScoreDisplay = document.querySelector('#computerScore');
+    const drawScoreDisplay = document.querySelector('#drawScore');
+
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+    drawScoreDisplay.textContent = drawScore;
+}
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const result = playRockPaperScissor(button.id, getComputerChoice());
+
+        const historyDisplay = document.querySelector('#history');
+        const roundResultDisplay = document.createElement('li');
+        roundResultDisplay.classList.add('log');
+        roundResultDisplay.textContent = result;
+
+        historyDisplay.appendChild(roundResultDisplay);
+
+        updateDisplay();
+    });
+});
